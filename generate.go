605d55ee4f8d624aed940072bcaa21a8cd335981
John Jeffery <jjeffery@sp.com.au>
2016-06-19 11:41:29 +1000

    Move wsdl file into wsdl directory

--- generate.go
+++ generate.go
@@ -3,4 +3,4 @@ package mturk
 // This documents the command used to generate the WSDL types.
 // See comment in wsdltypes.go regarding hand-editing of generated output.
 
-//go:generate gowsdl -p mturk -o wsdltypes.go AWSMechanicalTurkRequester-2012-03-25.wsdl
+//go:generate gowsdl -p mturk -o wsdltypes.go wsdl/AWSMechanicalTurkRequester-2012-03-25.wsdl
